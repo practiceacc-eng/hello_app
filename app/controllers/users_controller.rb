7ba0ba1a83fe43a1148e7fee833c62a32e55cb2b
practiceacc-eng <43659139+aleph-aseffa@users.noreply.github.com>
2021-03-16 22:32:50 -0500

    Added Team Generator functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  #def team
+  #  @users = User.all
+  #end
+
   def add
     @user = User.new
   end
@@ -32,6 +36,29 @@ class UsersController < ApplicationController
   # credit: https://stackoverflow.com/questions/17450185/forbidden-attributes-error-in-rails-4-when-encountering-a-situation-where-one-wo
   def user_params
     params.require(:user).permit(:player, :team, :points, :cost, :position)
+  end
+
+  # randomly generate a team
+  def team
+    num_selected = 0
+    @selected_players = []
+    while num_selected < 11
+      max_id = User.order(id: :desc).limit(1).pluck(:id).first
+      random_player = User.find_by('id > ?', rand(max_id))
+      # check if all the values are filled in, if not, select another player.
+      valid_player = true
+      valid_player = ((random_player.player == "") or valid_player)
+      valid_player = ((random_player.team == "") or valid_player)
+      valid_player = ((random_player.position == "") or valid_player)
+
+      if valid_player
+        @selected_players.append(random_player)
+        num_selected += 1
+      end
+    end
+
+    print(@selected_players)
+    @selected_players
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/roll', to: 'static_pages#roll'
+  get '/team', to: 'users#team'
   get '/index', to: 'users#index'
   get '/add', to: 'users#add'
 
